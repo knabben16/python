4f3a70082d845313c670b6af5512ce9bac4483c3
Vinícius <125661337+knabben16@users.noreply.github.com>
2023-07-11 10:28:21 -0300

    Add files via upload

---
+++ alura.py3/cliente.py
@@ -0,0 +1,14 @@
+class Cliente:
+
+    def __init__(self, nome):
+        self.__nome = nome
+
+    @property
+    def get_nome(self):
+        print("chamando @property nome()")
+        return self.__nome.title()
+    
+    @nome.setter
+    def nome(self, nome):
+        print("chamando setter nome()")
+        self.__nome = nome
